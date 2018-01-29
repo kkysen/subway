10867a1593d7328b8f417c4dc53fd908289d84ae
Khyber Sen <kkysen@gmail.com>
2018-01-29 02:08:33 -0500

    set up SpringBoot project

---
+++ src/main/java/sen/khyber/subway/SubwayApplication.java
@@ -0,0 +1,13 @@
+package sen.khyber.subway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayApplication {
+    
+    public static void main(final String[] args) {
+        SpringApplication.run(SubwayApplication.class, args);
+    }
+    
+}
